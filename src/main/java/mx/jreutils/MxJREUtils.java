aa03f07e81100833e070ffdf67a253a47a16d327
mroh004 <mroh004@a4eeb7b0-894d-ad40-892e-885a5e095833>
2011-11-17 01:58:46 +0000

    cleanup for gwt compilation

--- src/main/java/mx/jreutils/MxJREUtils.java
+++ src/main/java/mx/jreutils/MxJREUtils.java
@@ -1,8 +1,14 @@
 package mx.jreutils;
 
+import static mx.gwtutils.MxroGWTUtils.emptyOrNull;
+import static mx.gwtutils.MxroGWTUtils.flip;
+
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 
 public class MxJREUtils {
 
@@ -50,6 +56,75 @@ public class MxJREUtils {
 			start++;
 		}
 		return start;
+	}
+	
+	/**
+	 * 
+	 * Exceptions:<br/>
+	 * 
+	 * <li>Default ports will not be made part of the path. eg.
+	 * http://www.mxro.de:80/test/test1.xml?page=1&parameter=space+1#home will
+	 * return de/mxro/www/http/test/test1.xml_page_1_parameter_space_1_home and
+	 * not de/mxro/www/http/<strong>80</strong>/test/test1.
+	 * xml_page_1_parameter_space_1_home</li>
+	 * 
+	 * 
+	 */
+	public final static String getFileForURI(final String suri) {
+
+		try {
+			final URI uri = new URI(suri);
+
+			assert uri.isAbsolute() : "Cannot determine directory for node: URI must be absolute: <"
+					+ suri + ">";
+
+			// example
+			// http://www.mxro.de:80/test/test1.xml?page=1&parameter=space+1#home
+			final String scheme = uri.getScheme(); // eg <http>
+			final String host = uri.getHost(); // eg <www.mxro.de>
+			String port = String.valueOf(uri.getPort()); // eg <80>
+			if (port.equals("-1"))
+				port = "";
+			final String path = uri.getPath(); // eg </test/test1.xml>
+			final String query = uri.getQuery(); // eg <page=1>
+			final String fragment = uri.getFragment(); // eg <home>
+
+			assert !emptyOrNull(host) : "Cannot determine directory for node: host must be specified in URI: <"
+					+ suri + ">";
+
+			final StringBuffer dir = new StringBuffer();
+
+			// reverse the host eg www.mxro.de becomes de/mxro/www/
+			for (final String hostPart : flip(Arrays.asList(host.split("\\.")))) {
+				dir.append(hostPart + "/");
+			}
+
+			if (!emptyOrNull(scheme))
+				dir.append(scheme + "/");
+
+			if (!emptyOrNull(port)
+					&& !(port.equals("80") && scheme.equals("http")))
+				dir.append(port + "/");
+
+			if (!emptyOrNull(path) && path.length() > 2)
+				dir.append(path.substring(1));
+
+			if (!emptyOrNull(query))
+				dir.append("_"
+						+ query.replaceAll("=", "_").replaceAll("&", "_")
+								.replaceAll("\\+", "_"));
+
+			if (!emptyOrNull(fragment))
+				dir.append("_" + fragment);
+
+			return dir.toString();
+
+		} catch (final URISyntaxException e) {
+			throw new IllegalArgumentException(
+					"Cannot determine directory for node: no valid URI: <"
+							+ suri + ">");
+		}
+
 	}
 
 }
