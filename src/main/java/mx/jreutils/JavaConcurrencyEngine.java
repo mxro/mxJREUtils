c89fe24700d6fc4761e2e2309b27c399d99480f3
mxro <mxro@nowhere.com>
2015-01-09 10:46:50 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 10:46:49 EST 2015

--- src/main/java/mx/jreutils/JavaConcurrencyEngine.java
+++ src/main/java/mx/jreutils/JavaConcurrencyEngine.java
@@ -10,7 +10,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeoutException;
 
-import de.mxro.concurrency.schedule.ThreadSpace.Step;
+import de.mxro.concurrency.schedule.Step;
 import mx.gwtutils.ConcurrencyEngine;
 import mx.gwtutils.tests.AbstractTimer;
 
