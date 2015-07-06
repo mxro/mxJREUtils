b6ae2b81d1d7ad25f92b248b224e39c1038e46c5
mxro <mxro@nowhere.com>
2015-07-06 11:07:31 +1000

    Moving projects to Java Delight Autocommit Mon Jul 06 11:07:31 EST 2015

--- src/main/java/mx/jreutils/JavaConcurrencyEngine.java
+++ src/main/java/mx/jreutils/JavaConcurrencyEngine.java
@@ -5,12 +5,13 @@
  ******************************************************************************/
 package mx.jreutils;
 
+import delight.concurrency.schedule.Step;
+
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeoutException;
 
-import de.mxro.concurrency.schedule.Step;
 import mx.gwtutils.ConcurrencyEngine;
 import mx.gwtutils.tests.AbstractTimer;
 
